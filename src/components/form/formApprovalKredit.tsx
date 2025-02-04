9aa5e3608c9df8990c464f7bc815c75409ccda25
phanatagama <phanatagama@gmail.com>
2025-02-04 16:19:02 +0700

    feat: update form pengajuan

--- src/components/form/formApprovalKredit.tsx
+++ src/components/form/formApprovalKredit.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 
 const FormApprovalKredit: React.FC = () => {
     const dummyData = {
-        name: 'John Doe',
-        nik: '1234567890123456',
-        birthDate: '1990-01-01',
-        ktpPhoto: 'https://randomuser.me/api/portraits/men/1.jpg',
-        attachment: 'https://example.com/attachment.pdf'
+      name: "John Doe",
+      nik: "1234567890123456",
+      birthDate: "1990-01-01",
+      ktpPhoto:
+        "https://www.beritariau.com/foto_berita/2022/05/2022-05-23-aturan-baru-buat-ktp-nama-maksimal-60-huruf-termasuk-spasi.b686f703",
+      attachment: "https://example.com/attachment.pdf",
+      vehicle: {
+        brand: "Toyota",
+        model: "Avanza",
+        type: "MPV",
+        color: "Black",
+        price: "200,000,000",
+      },
+      loan: {
+        insuranceType: "Comprehensive",
+        downPayment: "50,000,000",
+        creditDuration: "5 bulan",
+        installmentPeriod: "10 bulan",
+      },
     };
 
     return (
@@ -37,6 +51,33 @@ const FormApprovalKredit: React.FC = () => {
                     <a href={dummyData.attachment} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
                         Download Attachment
                     </a>
+                </div>
+                <div className="text-white text-left text-base">
+                    <strong>Merk Kendaraan:</strong> {dummyData.vehicle.brand}
+                </div>
+                <div className="text-white text-left text-base">
+                    <strong>Model Kendaraan:</strong> {dummyData.vehicle.model}
+                </div>
+                <div className="text-white text-left text-base">
+                    <strong>Tipe Kendaraan:</strong> {dummyData.vehicle.type}
+                </div>
+                <div className="text-white text-left text-base">
+                    <strong>Warna Kendaraan:</strong> {dummyData.vehicle.color}
+                </div>
+                <div className="text-white text-left text-base">
+                    <strong>Harga Kendaraan:</strong> {dummyData.vehicle.price}
+                </div>
+                <div className="text-white text-left text-base">
+                    <strong>Tipe Asuransi:</strong> {dummyData.loan.insuranceType}
+                </div>
+                <div className="text-white text-left text-base">
+                    <strong>Down Payment:</strong> {dummyData.loan.downPayment}
+                </div>
+                <div className="text-white text-left text-base">
+                    <strong>Lama Kredit:</strong> {dummyData.loan.creditDuration}
+                </div>
+                <div className="text-white text-left text-base">
+                    <strong>Lama Angsuran:</strong> {dummyData.loan.installmentPeriod}
                 </div>
                 <button className="hover:cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
                     Approve
--- src/components/form/formPengajuanKredit.tsx
+++ src/components/form/formPengajuanKredit.tsx
@@ -1,23 +1,32 @@
 
 import React, { useState } from 'react';
+
 const FormPengajuanKredit = () => {
     const [formData, setFormData] = useState({
-        name: "",
-        nik: "",
-        contact: "",
-        idCardImage: null,
-        spkImage: null,
-        buktiBayarImage: null,
-        tanggal: "",
+      name: "",
+      nik: "",
+      contact: "",
+      idCardImage: null,
+      spkImage: null,
+      buktiBayarImage: null,
+      tanggal: "",
+      merkKendaraan: "",
+      modelKendaraan: "",
+      tipeKendaraan: "",
+      hargaKendaraan: "",
+      pilihanAsuransi: "",
+      pilihanDownPayment: "",
+      lamaKredit: "",
+      angsuran: "",
     });
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    };
+    // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const { name, value } = e.target;
+    //     setFormData({
+    //         ...formData,
+    //         [name]: value,
+    //     });
+    // };
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, files } = e.target;
@@ -34,40 +43,196 @@ const FormPengajuanKredit = () => {
         // Handle form submission
     };
 
-    return (
-        <form onSubmit={handleSubmit} className="space-y-4 p-4  shadow-md rounded-md">
-            <h1>Data Konsumen</h1>
-            <div className="flex flex-col">
-                <label className="mb-2 font-semibold text-left">Name:</label>
-                <input type="text" name="name" value={formData.name} onChange={handleChange} className="p-2 border rounded-md" />
-            </div>
-            <div className="flex flex-col">
-                <label className="mb-2 font-semibold text-left">NIK:</label>
-                <input type="nik" name="nik" value={formData.nik} onChange={handleChange} className="p-2 border rounded-md" />
-            </div>
-            <div className="flex flex-col">
-                <label className="mb-2 font-semibold text-left">Tanggal:</label>
-                <input type="date" name="tanggal" value={formData.tanggal} onChange={handleChange} className="p-2 border rounded-md" />
-            </div>
+    
 
-            <div className="flex flex-col">
-                <label className="mb-2 font-semibold text-left">Contact:</label>
-                <input type="contact" name="contact" value={formData.contact} onChange={handleChange} className="p-2 border rounded-md" />
-            </div>
-            <div className="flex flex-col">
-                <label className="mb-2 font-semibold text-left">ID Card Image:</label>
-                <input type="file" name="idCardImage" onChange={handleFileChange} className="p-2 border rounded-md" />
-            </div>
-            <div className="flex flex-col">
-                <label className="mb-2 font-semibold text-left">SPK Image:</label>
-                <input type="file" name="spkImage" onChange={handleFileChange} className="p-2 border rounded-md" />
-            </div>
-            <div className="flex flex-col">
-                <label className="mb-2 font-semibold text-left">Bukti Bayar Image:</label>
-                <input type="file" name="buktiBayarImage" onChange={handleFileChange} className="p-2 border rounded-md" />
-            </div>
-            <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">Submit</button>
-        </form>
+    const handleChange = (
+      e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    ) => {
+      const { name, value } = e.target;
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    };
+
+    return (
+      <form
+        onSubmit={handleSubmit}
+        className="space-y-4 p-4 shadow-md rounded-md"
+      >
+        <h1 className='font-semibold text-2xl'>Data Konsumen</h1>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">NIK:</label>
+          <input
+            type="text"
+            name="nik"
+            value={formData.nik}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">Tanggal:</label>
+          <input
+            type="date"
+            name="tanggal"
+            value={formData.tanggal}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">Contact:</label>
+          <input
+            type="text"
+            name="contact"
+            value={formData.contact}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">ID Card Image:</label>
+          <input
+            type="file"
+            name="idCardImage"
+            onChange={handleFileChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">SPK Image:</label>
+          <input
+            type="file"
+            name="spkImage"
+            onChange={handleFileChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">
+            Bukti Bayar Image:
+          </label>
+          <input
+            type="file"
+            name="buktiBayarImage"
+            onChange={handleFileChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <h1 className='font-semibold text-2xl'>Data Kendaraan</h1>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">
+            Merk Kendaraan:
+          </label>
+          <input
+            type="text"
+            name="merkKendaraan"
+            value={formData.merkKendaraan}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">
+            Model Kendaraan:
+          </label>
+          <input
+            type="text"
+            name="modelKendaraan"
+            value={formData.modelKendaraan}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">
+            Tipe Kendaraan:
+          </label>
+          <input
+            type="text"
+            name="tipeKendaraan"
+            value={formData.tipeKendaraan}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">
+            Harga Kendaraan:
+          </label>
+          <input
+            type="text"
+            name="hargaKendaraan"
+            value={formData.hargaKendaraan}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <h1 className='font-semibold text-2xl'>Data Pinjaman</h1>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">
+            Pilihan Asuransi:
+          </label>
+          <select
+            name="pilihanAsuransi"
+            value={formData.pilihanAsuransi}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          >
+            <option className='text-black' value="asuransi1">Asuransi 1</option>
+            <option className='text-black' value="asuransi2">Asuransi 2</option>
+          </select>
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">
+            Pilihan Down Payment:
+          </label>
+          <input
+            type="text"
+            name="pilihanDownPayment"
+            value={formData.pilihanDownPayment}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">Lama Kredit(bulan):</label>
+          <input
+            type="text"
+            name="lamaKredit"
+            value={formData.lamaKredit}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-2 font-semibold text-left">Angsuran(bulan):</label>
+          <input
+            type="text"
+            name="angsuran"
+            value={formData.angsuran}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          />
+        </div>
+        <button
+          type="submit"
+          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+        >
+          Submit
+        </button>
+      </form>
     );
         
 };
